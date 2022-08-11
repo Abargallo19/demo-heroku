7dda04e63c7afae12fcbe3a699c0b5aa944ef535
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-08-11 12:32:31 -0400

    get and push built

--- api/server.js
+++ api/server.js
@@ -3,3 +3,11 @@ const express = require('express');
 const server = express();
 
 module.exports = server;
+
+server.get('/', (req, res) => {
+    res.send('Aloha!');
+});
+
+server.put('/', (req, res)=> {
+    res.send('Hello from put');
+});
